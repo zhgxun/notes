82415782dde9844f407f4790ef1d592babe67448
zhgxun <zhgxun1989@163.com>
2019-01-23 22:06:04 +0800

    commit all change

--- java/learn/src/main/java/com/github/zhgxun/learn/common/xml/dom4j/Test2.java
+++ java/learn/src/main/java/com/github/zhgxun/learn/common/xml/dom4j/Test2.java
@@ -2,11 +2,13 @@ package com.github.zhgxun.learn.common.xml.dom4j;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.io.FileUtils;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 
+import java.io.File;
 import java.util.List;
 
 public class Test2 {
@@ -80,171 +82,7 @@ public class Test2 {
     }
 
     public static void main(String[] args) throws Exception {
-
-        System.out.println(xml2Json("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
-                "<urlset>\n" +
-                "  <url>\n" +
-                "    <loc>http://detail.zol.com.cn/series/3/17922_1.html</loc>  \n" +
-                "    <lastmod>2019-01-16</lastmod>  \n" +
-                "    <changefreq>always</changefreq>  \n" +
-                "    <priority>1.0</priority>\n" +
-                "    <data> \n" +
-                "      <display> \n" +
-                "        <title>FURY 报价 参数 图片 评论 ZOL中关村在线</title>  \n" +
-                "        <brand>金士顿</brand>  \n" +
-                "        <brandOther>kingston</brandOther>  \n" +
-                "        <serie>FURY</serie>  \n" +
-                "        <serieOther>fury系列</serieOther>  \n" +
-                "        <category>内存</category>  \n" +
-                "        <imgSrc>http://2b.zol-img.com.cn/product/138_120x90/627/ce5aNu1nFSx3o.jpg</imgSrc>  \n" +
-                "        <imgUrl>http://detail.zol.com.cn/series/3/17922_1.html</imgUrl>  \n" +
-                "        <price>￥299-3199</price>  \n" +
-                "        <param> \n" +
-                "          <key>适用类型</key>  \n" +
-                "          <value>台式机</value> \n" +
-                "        </param>  \n" +
-                "        <param> \n" +
-                "          <key>内存容量</key>  \n" +
-                "          <value>8GB</value> \n" +
-                "        </param>  \n" +
-                "        <param> \n" +
-                "          <key>容量描述</key>  \n" +
-                "          <value>单条(8GB)</value> \n" +
-                "        </param>  \n" +
-                "        <param> \n" +
-                "          <key>内存类型</key>  \n" +
-                "          <value>DDR3</value> \n" +
-                "        </param>  \n" +
-                "        <link> \n" +
-                "          <text>购买</text>  \n" +
-                "          <url>http://detail.zol.com.cn/386/385848/zplus.shtml</url> \n" +
-                "        </link>  \n" +
-                "        <link> \n" +
-                "          <text>详细参数</text>  \n" +
-                "          <url>http://detail.zol.com.cn/series/3/236/param_17922_0_1.html</url> \n" +
-                "        </link>  \n" +
-                "        <link> \n" +
-                "          <text>点评</text>  \n" +
-                "          <url>http://detail.zol.com.cn/series/3/236/review_17922.html</url> \n" +
-                "        </link>  \n" +
-                "        <link> \n" +
-                "          <text>问答</text>  \n" +
-                "          <url>http://ask.zol.com.cn/product_385848.html</url> \n" +
-                "        </link>  \n" +
-                "        <link> \n" +
-                "          <text>二手</text>  \n" +
-                "          <url>http://detail.zol.com.cn/series/3/236/ershou_17922.html</url> \n" +
-                "        </link>  \n" +
-                "        <link> \n" +
-                "          <text>视频</text>  \n" +
-                "          <url>http://detail.zol.com.cn/series/3/236/video_17922.html</url> \n" +
-                "        </link>  \n" +
-                "        <link> \n" +
-                "          <text>配件</text>  \n" +
-                "          <url>http://detail.zol.com.cn/series/3/236/fitting_17922.html</url> \n" +
-                "        </link>  \n" +
-                "        <hotModel> \n" +
-                "          <head>型号</head>  \n" +
-                "          <head>适用类型</head>  \n" +
-                "          <head>价格</head>  \n" +
-                "          <row> \n" +
-                "            <column> \n" +
-                "              <text>金士顿 骇客神条FURY 8GB DDR4 2400 HX424C15FB/8</text>  \n" +
-                "              <url>http://detail.zol.com.cn/memory/index1115558.shtml</url> \n" +
-                "            </column>  \n" +
-                "            <column> \n" +
-                "              <text>台式机</text> \n" +
-                "            </column>  \n" +
-                "            <column> \n" +
-                "              <text>￥469</text> \n" +
-                "            </column> \n" +
-                "          </row>  \n" +
-                "          <row> \n" +
-                "            <column> \n" +
-                "              <text>金士顿 骇客神条FURY 16GB DDR4 2400 HX424C15FB/16</text>  \n" +
-                "              <url>http://detail.zol.com.cn/memory/index1149796.shtml</url> \n" +
-                "            </column>  \n" +
-                "            <column> \n" +
-                "              <text>台式机</text> \n" +
-                "            </column>  \n" +
-                "            <column> \n" +
-                "              <text>￥999</text> \n" +
-                "            </column> \n" +
-                "          </row>  \n" +
-                "          <row> \n" +
-                "            <column> \n" +
-                "              <text>金士顿 骇客神条FURY 8GB DDR3 1866</text>  \n" +
-                "              <url>http://detail.zol.com.cn/memory/index385848.shtml</url> \n" +
-                "            </column>  \n" +
-                "            <column> \n" +
-                "              <text>台式机</text> \n" +
-                "            </column>  \n" +
-                "            <column> \n" +
-                "              <text>￥499</text> \n" +
-                "            </column> \n" +
-                "          </row>  \n" +
-                "          <more> \n" +
-                "            <text>查看FURY全部产品</text>  \n" +
-                "            <url>http://detail.zol.com.cn/series/3/17922_1.html</url> \n" +
-                "          </more> \n" +
-                "        </hotModel>  \n" +
-                "        <publicPraise> \n" +
-                "          <score>4.8</score>  \n" +
-                "          <newScore>9.7</newScore>  \n" +
-                "          <count>158</count>  \n" +
-                "          <advantage> \n" +
-                "            <text>性价比高</text>  \n" +
-                "            <count>1</count> \n" +
-                "          </advantage>  \n" +
-                "          <disAdvantage> \n" +
-                "            <text>暂无</text>  \n" +
-                "            <count>0</count> \n" +
-                "          </disAdvantage>  \n" +
-                "          <good> \n" +
-                "            <count>153</count>  \n" +
-                "            <url>http://detail.zol.com.cn/386/385848/review.shtml#r3</url> \n" +
-                "          </good>  \n" +
-                "          <medium> \n" +
-                "            <count>3</count>  \n" +
-                "            <url>http://detail.zol.com.cn/386/385848/review.shtml#r4</url> \n" +
-                "          </medium>  \n" +
-                "          <bad> \n" +
-                "            <count>2</count>  \n" +
-                "            <url>http://detail.zol.com.cn/386/385848/review.shtml#r5</url> \n" +
-                "          </bad>  \n" +
-                "          <more> \n" +
-                "            <text>查看FURY更多口碑评论</text>  \n" +
-                "            <url>http://detail.zol.com.cn/386/385848/review.shtml</url> \n" +
-                "          </more> \n" +
-                "        </publicPraise>  \n" +
-                "        <competingProducts> \n" +
-                "          <row> \n" +
-                "            <linkText>影驰GAMER 8GB DDR4 2400</linkText>  \n" +
-                "            <linkUrl>http://detail.zol.com.cn/memory/index1147009.shtml</linkUrl>  \n" +
-                "            <price>￥459</price>  \n" +
-                "            <compareUrl>http://detail.zol.com.cn/pk/385848_1147009.shtml</compareUrl> \n" +
-                "          </row>  \n" +
-                "          <row> \n" +
-                "            <linkText>金士顿8GB DDR3 1600 KVR1</linkText>  \n" +
-                "            <linkUrl>http://detail.zol.com.cn/memory/index328918.shtml</linkUrl>  \n" +
-                "            <price>￥439</price>  \n" +
-                "            <compareUrl>http://detail.zol.com.cn/pk/328918_385848.shtml</compareUrl> \n" +
-                "          </row>  \n" +
-                "          <row> \n" +
-                "            <linkText>影驰GAMER Ⅱ 8GB DDR4 24</linkText>  \n" +
-                "            <linkUrl>http://detail.zol.com.cn/memory/index1207114.shtml</linkUrl>  \n" +
-                "            <price>￥475</price>  \n" +
-                "            <compareUrl>http://detail.zol.com.cn/pk/385848_1207114.shtml</compareUrl> \n" +
-                "          </row>  \n" +
-                "          <more> \n" +
-                "            <text>查看FURY更多产品</text>  \n" +
-                "            <url>http://detail.zol.com.cn/series/3/17922_1.html</url> \n" +
-                "          </more> \n" +
-                "        </competingProducts>  \n" +
-                "        <showUrl>detail.zol.com.cn</showUrl>\n" +
-                "      </display>\n" +
-                "    </data>\n" +
-                "  </url> \n" +
-                "</urlset>"));
+        String content = FileUtils.readFileToString(new File("/Users/zhgxun/Desktop/test.xml"), "UTF-8");
+        System.out.println(xml2Json(content));
     }
 }
