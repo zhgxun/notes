504ad160a4986b09ec7ee6772c623098dd56a463
zhgxun <zhgxun1989@163.com>
2019-02-28 00:43:03 +0800

    链表学习

--- java/banana/src/main/java/github/banana/letcode/DeleteDuplicates.java
+++ java/banana/src/main/java/github/banana/letcode/DeleteDuplicates.java
@@ -1,26 +1,100 @@
 package github.banana.letcode;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class DeleteDuplicates {
 
     public static void main(String[] args) {
+        // 1 2 3 3 4 4 5
+        ListNode node = new ListNode(1);
+//        node.next = new ListNode(2);
+//        node.next.next = new ListNode(3);
+//        node.next.next.next = new ListNode(3);
+//        node.next.next.next.next = new ListNode(4);
+//        node.next.next.next.next.next = new ListNode(4);
+//        node.next.next.next.next.next.next = new ListNode(5);
 
+        node.next = new ListNode(1);
+        node.next.next = new ListNode(1);
+        node.next.next.next = new ListNode(1);
+        node.next.next.next.next = new ListNode(2);
+        node.next.next.next.next.next = new ListNode(3);
+
+        ListNode node2 = node;
+        System.out.println("原始链表...");
+        while (node2 != null) {
+            System.out.print(node2.val + " ");
+            node2 = node2.next;
+        }
+        System.out.println(" ");
+
+        ListNode node1 = deleteDuplicates2(node);
+        System.out.println("删除后的链表...");
+        while (node1 != null) {
+            System.out.print(node1.val + " ");
+            node1 = node1.next;
+        }
+        System.out.println(" ");
     }
 
     public ListNode deleteDuplicates(ListNode head) {
-        int count = 0;
-        ListNode slow = head, fast = head.next;
-        while (slow != null) {
-            count++;
-            if (slow.val != fast.val) {
-                if (count > 1) {
-                    slow.next = fast;
-                    count = 0;
-                }
-                slow = slow.next;
+        ListNode node = head;
+        while (node != null && node.next != null) {
+            if (node.val == node.next.val) {
+                node.next = node.next.next;
+            } else {
+                node = node.next;
             }
-            fast = fast.next;
+        }
+        return head;
+    }
+
+    /**
+     * 保存的方式为用一个新的链表来保存无重复的链表
+     *
+     * @param head 原始链表头
+     * @return 新链表头
+     */
+    public static ListNode deleteDuplicates2(ListNode head) {
+        if (head == null) {
+            return null;
         }
 
-        return head;
+        // 虚幻的链表头
+        ListNode node = new ListNode(-1);
+
+        ListNode slow = head;
+        ListNode fast;
+
+        // 记住头指针
+        head = node;
+
+        while (slow != null) {
+            boolean flag = false;
+
+            // 始终是上一个链表的后一个指针
+            fast = slow.next;
+            // 找重复值
+            while (fast != null && fast.val == slow.val) {
+                flag = true;
+                // 记录当前的节点
+                fast = fast.next;
+            }
+
+            // 当前值无重复时直接将链表拼接到 node 上
+            // 注意保存的方式
+            if (!flag) {
+                node.next = slow;
+                node = slow;
+            }
+
+            // 快指针位置
+            slow = fast;
+        }
+
+        node.next = null;
+
+        return head.next;
     }
 }
---
+++ java/banana/src/main/java/github/banana/letcode/InsertionSortList.java
@@ -0,0 +1,82 @@
+package github.banana.letcode;
+
+public class InsertionSortList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode dummyHead = new ListNode(0);
+        dummyHead.next = head;
+
+        // head~pre是排好序的部分
+        // pre 是有序链表的最后一个节点
+        ListNode pre = head;
+
+        // 第一个元素默认是有序的
+        // 从第二个元素开始比较
+        ListNode cur = head.next;
+        while (cur != null) {
+            // 寻找插入位置
+            // 每次都从头开始遍历, 寻找第一次大于当前节点的位置
+            // 说明当前元素应该插入该点的前面
+            // 即是 cur 要追加到 insertPre 的后面
+            ListNode insertPre = findInsertIndexPre(dummyHead, cur);
+
+            // 这种情况表示当前节点不需要换位置
+            // 两个节点重合, 是同一个数或者已排序链表的末尾
+            // 将最新的有序末尾指向当前即可
+            // 该部分保持有序
+            // 继续前进
+            if (insertPre == pre) {
+                pre = cur;
+                cur = cur.next;
+            } else {
+                // cur的需要插入到insertPre后面的位置
+                /*
+                 *                      4(cur)
+                 *                      |
+                 *                      v
+                 * -> 1 -> 3(insertPre) -> 5(pre) ->
+                 *
+                 * 就是说 4 要插入到 3 和 5 之间
+                 * 首先将 5 即是上一个有序链表末尾 pre 的下一个指向当前的 cur 的下一个
+                 * 其次 cur 的下一个其实就是 pre, 然而 cur.next 无法直接指向 pre, 需要借助 pre 的上一个 insertPre.next 来指向
+                 * 最后 找到 cur 的上一个节点, 其实就是 insertPre, 直接指向即可 insertPre.next = cur
+                 * 这样就完成了3个链表节点的交换
+                 * 还是比较烧脑的, 链表就这点特别复杂, 弄错就互相找不到了
+                 */
+                pre.next = cur.next;
+                cur.next = insertPre.next;
+                insertPre.next = cur;
+
+                // 移动cur
+                cur = pre.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+    /**
+     * 查找cur要插入位置的前一个节点
+     *
+     * @param head
+     * @param cur
+     * @return
+     */
+    private ListNode findInsertIndexPre(ListNode head, ListNode cur) {
+        while (head.next != cur) {
+            if (head.next.val >= cur.val) {
+                return head;
+            }
+            head = head.next;
+        }
+        return head;
+    }
+}
