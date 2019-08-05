5465b5cf4e11ab59efdadb245d7b02e63e91497a
zhgxun1989 <zhgxun1989@163.com>
2019-08-05 22:04:05 +0800

    add all

--- java/banana/src/main/java/github/banana/tomcat/SelF2Servlet.java
+++ java/banana/src/main/java/github/banana/tomcat/SelF2Servlet.java
@@ -1,4 +1,24 @@
 package github.banana.tomcat;
 
-public class SelF2Servlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/self2")
+public class SelF2Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("Content-Type: application/json;charset=UTF-8");
+        resp.getWriter().println("{\"name\":\"张三\",\"desc\":\"这是一段描述\",\"type\":\"get2\"}");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("Content-Type: application/json;charset=UTF-8");
+        resp.getWriter().println("{\"name\":\"张三\",\"desc\":\"这是一段描述\",\"type\":\"post2\"}");
+    }
 }
--- java/banana/src/main/java/github/banana/tomcat/SelfServlet.java
+++ java/banana/src/main/java/github/banana/tomcat/SelfServlet.java
@@ -1,4 +1,31 @@
 package github.banana.tomcat;
 
-public class SelfServlet {
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 实现一个自定义的 Servlet 容器
+ */
+public class SelfServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Get请求示例");
+        // 默认未被实现, 需要自行实现
+        // super.doGet(req, resp);
+        resp.setContentType("Content-Type: application/json;charset=UTF-8");
+        resp.getWriter().println("{\"name\":\"张三\",\"desc\":\"这是一段描述\",\"type\":\"get2\"}");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Post请求示例");
+        // 默认未被实现, 需要自行实现
+        // super.doPost(req, resp);
+        resp.setContentType("Content-Type: application/json;charset=UTF-8");
+        resp.getWriter().println("{\"name\":\"张三\",\"desc\":\"这是一段描述\",\"type\":\"post2\"}");
+    }
 }
