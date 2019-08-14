0936c55489c315aa240f6775b8c0fcedac2acb15
zhgxun1989 <zhgxun1989@163.com>
2019-08-14 23:10:14 +0800

    初始化socket

---
+++ c/socket/cmake-build-debug/CMakeFiles/socket.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/socket.dir/library.c.o"
+  "libsocket.pdb"
+  "libsocket.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/socket.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ c/socket/library.h
@@ -0,0 +1,6 @@
+#ifndef SOCKET_LIBRARY_H
+#define SOCKET_LIBRARY_H
+
+void hello(void);
+
+#endif //SOCKET_LIBRARY_H
